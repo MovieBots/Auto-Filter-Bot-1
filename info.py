c21e41efbbfb454025153d11b5dcd7a2eeb3799b
MovieBots <154045205+MovieBots@users.noreply.github.com>
2025-09-23 08:56:40 +0530

    Update info.py

--- info.py
+++ info.py
@@ -51,9 +51,9 @@ MULTIPLE_DB = is_enabled(os.environ.get('MULTIPLE_DB', "False"), False) # Type T
 DATABASE_URI2 = environ.get('DATABASE_URI2', "")
 
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+lwmT_ESNTLlkMTJl')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/Film_CityOfficial')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+m2-CnUFU_5c1MDQ1')
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/Arpitbotmovies')
-UPDATE_CHANNEL_LNK = environ.get('UPDATE_CHANNEL_LNK', 'https://t.me/Film_CityOfficial')
+UPDATE_CHANNEL_LNK = environ.get('UPDATE_CHANNEL_LNK', 'https://t.me/+m2-CnUFU_5c1MDQ1')
 
 AUTH_CHANNEL = environ.get("AUTH_CHANNEL", "-1003035165539 -1002834808662") # add multiple channels here, separated by single space
 AUTH_CHANNEL = [int(ch) for ch in AUTH_CHANNEL.strip().split()] if AUTH_CHANNEL else []
